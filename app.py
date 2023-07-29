f1b3c60416350f6f92bc8a45ddda1e42d700e3b4
Kovács Máté <kovacsmate1111@gmail.com>
2023-07-29 18:09:06 +0200

    Add app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+def swap_case(string):
+    return string.swapcase()
+
+@app.route('/', methods=['POST'])
+def swap_case_handler():
+    try:
+        data = request.get_json()
+        string = data.get('string')
+
+        if string is None or not isinstance(string, str):
+            raise ValueError("You did not specify a parameter or it is not string")
+
+        result = swap_case(string)
+        response = {'swapped_string': result}
+        return json.dumps(response), 200, {'Content-Type': 'application/json'}
+
+    except Exception as e:
+        error_response = {'error': str(e)}
+        return json.dumps(error_response), 400, {'Content-Type': 'application/json'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
